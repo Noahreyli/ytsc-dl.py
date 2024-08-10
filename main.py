d372e48faa6ada5437c1b81eca13273c7d2a0b69
Rucyy <127585375+Rucyy@users.noreply.github.com>
2024-08-10 22:21:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
-import asyncio
 import os
-from sclib.asyncio import SoundcloudAPI, Track, Playlist
+import time
+import subprocess
 
-api = SoundcloudAPI()  
+from pytube import Playlist, YouTube
 
-async def get_track():
-    track = await api.resolve('https://soundcloud.com/casso-wav/prada')
-    assert type(track) is Track
-    return track
+def run_playlist(pl):
+    for video in pl:
+        yt = YouTube(video)
+        music = yt.streams.first()
 
+        default_filename = music.default_filename.replace(' ', '-').replace('   ', '-').replace('(', '-').replace(')', '-')
+        print("Downloading " + default_filename + "...")
+        
+        yt.streams.first().download(filename=default_filename)
+        time.sleep(1)
+        
+        new_filename = default_filename[0:-3] + "mp3"
+        print("### Converting to MP3 ###")
+        
+        ffmpeg = ('ffmpeg -i '+default_filename[0:-4] + "mp4.mp4"+' '+new_filename)
+        subprocess.call(ffmpeg, shell=True)
 
-async def write_track_to_file(track, filename):
-    if not os.path.exists('./track_downloads'):
-        os.makedirs('./track_downloads')
-    with open(filename, 'wb+') as file:
-        await track.write_mp3_to(file)
+    print("Download finished.")
 
-track = asyncio.run(get_track())
-filename = f'./track_downloads/{track.artist} - {track.title}.mp3'
-asyncio.run(write_track_to_file(track, filename))
+def run_single(yt):
+    music = yt.streams.first()
+    default_filename = music.default_filename.replace(' ', '-').replace('   ', '-').replace('(', '-').replace(')', '-')
+    print("Downloading " + default_filename + "...")
+    yt.streams.first().download(filename=default_filename)
+    time.sleep(1)
+    new_filename = default_filename[0:-3] + "mp3"
+    print("### Converting to MP3 ###")
+
+    ffmpeg = ('ffmpeg -i ' + default_filename[0:-4] + 'mp4.mp4 ' + new_filename)
+    print(ffmpeg)
+    subprocess.call(ffmpeg, shell=True)
+
+    print("Download finished.")
+
+if __name__ == "__main__":
+    options = input("[1] Youtube Playlist - [2] Youtube Single - [3] SoundCloud Playlist ou Single: ")
+    if options == "1":
+        url = input("Please enter the url of the playlist you wish to download: ")
+        pl = Playlist(url)
+        print('Number of videos in playlist: %s' % len(pl.video_urls))
+        run_playlist(pl)
+    elif options == "2":
+        url = input("Please enter the url: ")
+        yt = YouTube(url)
+        run_single(yt)
+    elif options == "3":
+        url = input("Please enter the url SoundCloud: ")
+        scdl = ('scdl -l '+url)
+        subprocess.call(scdl, shell=True)
